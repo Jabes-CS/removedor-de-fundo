c68e54256a8aa0340498a8c0c58c74ce74bf69b0
Jabes-CS <jabes.candido1@gmail.com>
2026-03-10 17:31:46 -0300

    Validação de fotos png... para o script não quebrar

--- removedor.py
+++ removedor.py
@@ -5,17 +5,20 @@ import os
 input_folder = "img_input"
 output_folder = "img_output"
 
-# percorre todos os arquivos da pasta
+valid_extensions = (".png", ".jpg", ".jpeg")
+
 for file in os.listdir(input_folder):
 
-    input_path = os.path.join(input_folder, file)
-    output_path = os.path.join(output_folder, f"removed_{file}")
+    if file.lower().endswith(valid_extensions):
 
-    print(f"Processando: {file}")
+        input_path = os.path.join(input_folder, file)
+        output_path = os.path.join(output_folder, f"removed_{file}")
 
-    img = Image.open(input_path)
-    img_removed = remove(img)
+        print(f"Processando: {file}")
 
-    img_removed.save(output_path)
+        img = Image.open(input_path)
+        img_removed = remove(img)
+
+        img_removed.save(output_path)
 
 print("Todas as imagens foram processadas!")
