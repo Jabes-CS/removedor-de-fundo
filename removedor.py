2efbc2991e5375019330e9d673ec2597a748ea19
Jabes-CS <jabes.candido1@gmail.com>
2026-03-10 17:30:04 -0300

    trabalhar em cima de todas as fotos contidas na pasta

--- removedor.py
+++ removedor.py
@@ -1,10 +1,21 @@
 from rembg import remove
 from PIL import Image
+import os
 
-input_path = 'img_input/favicon.png'
-output_path = 'img_output/img_removed.png'
+input_folder = "img_input"
+output_folder = "img_output"
 
-img = Image.open(input_path)
-img_removed = remove(img)
+# percorre todos os arquivos da pasta
+for file in os.listdir(input_folder):
 
-img_removed.save(output_path)
+    input_path = os.path.join(input_folder, file)
+    output_path = os.path.join(output_folder, f"removed_{file}")
+
+    print(f"Processando: {file}")
+
+    img = Image.open(input_path)
+    img_removed = remove(img)
+
+    img_removed.save(output_path)
+
+print("Todas as imagens foram processadas!")
